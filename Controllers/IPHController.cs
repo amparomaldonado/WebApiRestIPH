b1ea3135f1c223a83f0fdffa9636c3a47b9d2b80
Amparo Fanny Maldonado Vázquez <amparo.maldonado@sspc.gob.mx>
2023-05-04 13:53:58 -0500

    commit

--- Controllers/IPHController.cs
+++ Controllers/IPHController.cs
@@ -1,18 +1,52 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+
+
 namespace WebApiRestIPH.Controllers
 {
     public class IPHController : ApiController
     {
+
+
         // GET api/values
-        public IEnumerable<string> Get()
+
+        [HttpGet]
+
+        /*   public ActionResult<Product> GetById_ActionResultOfT(int id)
+           {
+               var product = _productContext.Products.Find(id);
+               return product == null ? NotFound() : product;
+           }*/
+
+        public string Get()
+        //public ActionResult<List<IPH>>  Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            List<IPH> personas = new List<IPH>();
+            IPH per = new IPH();
+            per.idFuente = 3;
+            per.nombre = "JUAN";
+            per.paterno = "PEREZ";
+            per.materno = "MATINEZ";
+            per.curp = "MAVA750909MMSZLM01";
+            per.fechanac = "09/09/1975";
+
+            personas.Add(per);
+            per.idFuente = 3;
+            per.nombre = "JUAN";
+            per.paterno = "PEREZ";
+            per.materno = "MATINEZ";
+            per.curp = "MAVA750909MMSZLM01";
+            per.fechanac = "09/09/1975";
+            personas.Add(per);
+
+            string json = JsonConvert.SerializeObject(personas);
+            return json;
         }
 
         // GET api/values/5
@@ -35,5 +69,20 @@ namespace WebApiRestIPH.Controllers
         public void Delete(int id)
         {
         }
+
+
+
+        public class IPH
+        {
+            public int idFuente;
+            public string nombre;
+            public string paterno;
+            public string materno;
+            public string curp;
+            public string fechanac;
+        }
     }
+
+
+
 }
