5377e6d3f5cb16950900eebc62d97c344164dd2b
Amparo Fanny Maldonado Vázquez <amparo.maldonado@sspc.gob.mx>
2023-05-04 12:34:17 -0500

    commit 1

---
+++ Models/IPH.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiRestIPH.Models
+{
+    public class IPH
+    {
+        public int  idFuente;
+        public string nombre;
+        public string paterno;
+        public string materno;
+    }
+}
