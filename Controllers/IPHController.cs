2251028e3d5821119ac72889ea3598d5150f3564
Amparo Fanny Maldonado Vázquez <amparo.maldonado@sspc.gob.mx>
2023-05-04 13:11:34 -0500

    COMMIT

--- Controllers/IPHController.cs
+++ Controllers/IPHController.cs
@@ -9,10 +9,23 @@ namespace WebApiRestIPH.Controllers
 {
     public class IPHController : ApiController
     {
+
+
         // GET api/values
-        public IEnumerable<string> Get()
+        public List<IPH> Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            List<IPH> personas = new List<IPH>();
+            IPH per = new IPH();
+            per.idFuente = 3;
+            per.nombre = "JUAN";
+            per.paterno = "PEREZ";
+            per.materno = "MATINEZ";
+            per.curp = "MAVA750909MMSZLM01";
+            per.fechanac = "09/09/1975";
+
+            personas.Add(per);
+
+            return personas;
         }
 
         // GET api/values/5
@@ -35,5 +48,20 @@ namespace WebApiRestIPH.Controllers
         public void Delete(int id)
         {
         }
+
+
+
+        public class IPH
+        {
+            public int idFuente;
+            public string nombre;
+            public string paterno;
+            public string materno;
+            public string curp;
+            public string fechanac;
+        }
     }
+
+
+
 }
